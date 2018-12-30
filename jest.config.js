34892f1d04c80a52e14f9ac8a1bf007d3f4be1c2
Wim Selles <wim@wswebcreation.nl>
2018-12-30 21:17:37 +0100

    test: remove jasmine and add jest

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  roots: [
+    '<rootDir>/lib'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/node_modules/**',
+    '!**/*.spec.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 98,
+      functions: 98,
+      lines: 98,
+      statements: 98,
+    }
+  },
+  coverageDirectory: './coverage/',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+};
