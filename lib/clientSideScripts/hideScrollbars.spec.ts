5991a88938c293f5f65d42e60e01ef18c5554121
Wim Selles <wim@wswebcreation.nl>
2019-05-26 07:39:16 +0200

    fix: add overflow to body instead of html

--- lib/clientSideScripts/hideScrollbars.spec.ts
+++ lib/clientSideScripts/hideScrollbars.spec.ts
@@ -2,14 +2,14 @@ import hideScrollBars from './hideScrollbars';
 
 describe('hideScrollBars', ()=>{
   it('should be able to hide and show the scrollbars', ()=>{
-    expect(document.documentElement.style.overflow).toMatchSnapshot();
+    expect(document.body.style.overflow).toMatchSnapshot();
 
     hideScrollBars(true);
 
-    expect(document.documentElement.style.overflow).toMatchSnapshot();
+    expect(document.body.style.overflow).toMatchSnapshot();
 
     hideScrollBars(false);
 
-    expect(document.documentElement.style.overflow).toMatchSnapshot();
+    expect(document.body.style.overflow).toMatchSnapshot();
   });
 });
