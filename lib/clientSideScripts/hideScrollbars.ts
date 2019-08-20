8c303b6149e85c68cc69c5d6606481544afd3fc7
Wim Selles <wim@wswebcreation.nl>
2019-08-20 19:39:21 +0200

    fix: this fixes issue 30

--- lib/clientSideScripts/hideScrollbars.ts
+++ lib/clientSideScripts/hideScrollbars.ts
@@ -1,5 +1,9 @@
 /**
  * Hide the scrollbars
+ *
+ * There is a `try/catch` for this issue
+ * https://github.com/wswebcreation/webdriver-image-comparison/issues/30
+ * Some browsers don't wrap XML into a body element, so this will fail
  */
 export default function hideScrollBars(hide: boolean): void {
   try {
