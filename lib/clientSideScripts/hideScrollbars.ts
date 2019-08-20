918859b4b7f31719eafa2d4a381c3dfa9fe5530d
Wim Selles <wim@wswebcreation.nl>
2019-08-20 17:39:50 +0200

    fix: this fixes issue 30

--- lib/clientSideScripts/hideScrollbars.ts
+++ lib/clientSideScripts/hideScrollbars.ts
@@ -2,9 +2,13 @@
  * Hide the scrollbars
  */
 export default function hideScrollBars(hide: boolean): void {
-  if (hide) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = '';
+  try {
+    if (hide) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  } catch (e) {
+    // Do nothing
   }
 }
