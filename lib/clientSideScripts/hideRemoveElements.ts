53917a07318a2b7a1d1c38720d221786bfb3025c
Wim Selles <wim@wswebcreation.nl>
2020-02-29 20:58:52 +0100

    fix: fix issue 52
    - webelements needed to be converted to html elements
    - add webdriver types

--- lib/clientSideScripts/hideRemoveElements.ts
+++ lib/clientSideScripts/hideRemoveElements.ts
@@ -1,29 +1,45 @@
+import {WebElement} from 'selenium-webdriver';
+
 /**
  * Hide or remove elements on the page
  */
 export default function hideRemoveElements(
   hideRemoveElements:
     {
-      hide: (HTMLElement | HTMLElement[])[],
+      hide: (HTMLElement | HTMLElement[] | WebElement | WebElement[])[],
       remove: (HTMLElement | HTMLElement[])[],
     },
-  hideRemove: boolean): void {
+  hideRemove: boolean): any {
 
   hideRemoveElements.hide.forEach(element => {
     if (Array.isArray(element)) {
-      return element.forEach(singleElement => {
-        singleElement.style.visibility = hideRemove ? 'hidden' : '';
-      });
+      return element.forEach((singleElement: HTMLElement | WebElement) => hideRemoveEl(singleElement, 'visibility', hideRemove));
     }
-
-    return element.style.visibility = hideRemove ? 'hidden' : '';
+    hideRemoveEl(element, 'visibility', hideRemove, true);
   });
 
   hideRemoveElements.remove.forEach(element => {
     if (Array.isArray(element)) {
-      return element.forEach(singleElement => singleElement.style.display = hideRemove ? 'none' : '');
+      return element.forEach((singleElement: HTMLElement | WebElement) => hideRemoveEl(singleElement, 'display', hideRemove));
     }
-
-    return element.style.display = hideRemove ? 'none' : '';
+    hideRemoveEl(element, 'display', hideRemove, true);
   });
+
+  function hideRemoveEl(el: HTMLElement | WebElement, prop: string, hideRemove: boolean, singleElement: boolean = false) {
+    const value = prop === 'visibility' ? 'hidden' : 'none';
+    try {
+      // Here we get the HTMLElement, if this fails we have a WebElement
+      // @ts-ignore
+      el.style[prop] = hideRemove ? value : '';
+    } catch (e) {
+      /// Here we have the WebElement
+      if (singleElement) {
+        // @ts-ignore
+        return document.querySelector(el.selector).style[prop] = hideRemove ? value : '';
+      }
+
+      // @ts-ignore
+      return document.querySelectorAll(el.selector).forEach(singleEl => singleEl.style[prop] = hideRemove ? value : '');
+    }
+  }
 }
